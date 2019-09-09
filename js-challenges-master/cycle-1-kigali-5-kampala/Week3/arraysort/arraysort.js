4046c997a2ef39d5c492b68e77f55b015ec707f1
Cleopatra Kanyunyuzi <kanyunyuzicleo@gmail.com>
2019-09-08 23:35:09 -0700

    sort.1

---
+++ js-challenges-master/cycle-1-kigali-5-kampala/Week3/arraysort/arraysort.js
@@ -0,0 +1,29 @@
+
+
+var nums = [1,2,3,4,5,'a' ,'b','c']
+function arraySort(anArray) {
+    // Your code here
+    
+    var evens = [];
+    var odds = [];
+    var char = [];
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+//module.exports = arraySort;
