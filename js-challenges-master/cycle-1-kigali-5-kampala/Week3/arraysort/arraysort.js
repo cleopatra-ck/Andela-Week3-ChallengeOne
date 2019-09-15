2ad86c637d2fbb99f74f663fa639089f4f4e5569
Kanyunyuzi Cleopatra <kanyunyuzicleo@gmail.com>
2019-09-15 11:24:16 -0700

    Update arraysort.js

--- js-challenges-master/cycle-1-kigali-5-kampala/Week3/arraysort/arraysort.js
+++ js-challenges-master/cycle-1-kigali-5-kampala/Week3/arraysort/arraysort.js
@@ -1,39 +1,32 @@
 
 
-var nums = [1,2,'a',3,'b',4,'c',5,'e',6,'f', 7, 8];
-arraySort(nums);
-
 function arraySort(anArray) {
     // Your code here
+    if(Array.isArray(anArray)){
     
-    var evens = [];
-    var odds = [];
-    var chars = [];
-
-    for (var i = 0; i < anArray.length; i++)
-    {
-
-       if(anArray[i] % 2===0) {
-        evens.push(anArray[i]);
-     
-
-        } else if (anArray[i]%2===1) {
-        odds.push(anArray[i]);
-        } 
-        
-        else {
-        chars.push(anArray[i]);
-        }
- 
-   }
-
-   console.log('Evens' + " " + ":" + " " + [evens] );
-    console.log('Odds' + " " + ":" + " " + [odds]);
-    console.log('Chars' + " " + ":" + " " + [chars]);
-    
-  
-}
+        let arrob ={ evens:[], odds:[], chars:[]};
 
 
+        for(i = 0; i < anArray.length; i++) {
+          if(anArray[i]%2 == 0) {
+              arrob.evens .push(anArray[i]); 
+           }
+          else if (anArray[i]%2 == 1) {
+            arrob.odds.push(anArray[i]);
+          }
+          else {
+            arrob.chars.push(anArray[i]);
+          }  }
+      arrob.evens.sort();
+      arrob.odds.sort();
+      arrob.chars.sort();
+      return arrob;
 
+    }
+    else { return "invalid input"}
+    }
+
+       
 module.exports = arraySort;
+    
+ 
